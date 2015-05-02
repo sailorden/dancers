44b036b92c6252e4b2eccdc2fbfbaeb5d48e8e1f
techmsi <Sophie.Idromenos@nymag.com>
2015-05-02 11:42:49 -0400

    Server: Added variable port

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.use(function(err, req, res, next) {
 var server = app.listen(3000, function () {
 
   var host = server.address().address
-  var port = server.address().port
+  var port = process.env.PORT || server.address().port
 
   console.log('App listening at http://%s:%s', host, port)
 
